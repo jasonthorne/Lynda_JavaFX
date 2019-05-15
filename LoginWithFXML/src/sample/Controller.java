f558161eae99edc5056536c0ce9a16559053404a
JasonThorne <jasonthorne@live.ie>
2019-05-15 22:28:06 +0100

    Sample Applications - 6

--- LoginWithFXML/src/sample/Controller.java
+++ LoginWithFXML/src/sample/Controller.java
@@ -1,4 +1,28 @@
 package sample;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller{ //implements Initializable { //implementation of Initializable added by me!!
+
+    //============Added by me!
+    @FXML
+    private Text actiontarget; //Text that the program will set on click.
+
+    @FXML
+    private void handleSubmitButtonAction(ActionEvent event) { //action event handler for button
+        //System.out.println("You clicked it, baby!");
+        actiontarget.setText("Sign in button was pressed!");
+    }
+    //===========
+
+    //@Override
+   // public void initialize(URL location, ResourceBundle resources) {
+
+    //}
 }
