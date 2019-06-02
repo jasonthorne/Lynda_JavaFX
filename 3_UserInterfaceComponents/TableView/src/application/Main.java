d27c7455bb96e94c2815f0a60b839d897172090f
JasonThorne <jasonthorne@live.ie>
2019-06-02 16:29:09 +0100

    UI Components - 11

--- 3_UserInterfaceComponents/TableView/src/application/Main.java
+++ 3_UserInterfaceComponents/TableView/src/application/Main.java
@@ -1,12 +1,16 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
@@ -21,8 +25,11 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			
+			primaryStage.setTitle("Table View");
+			
 			//make labels:
 			Label response = new Label("");
+			response.setFont(Font.font("Arial", 14));
 			Label title = new Label("Contact list using a TableView\n");
 			title.setFont(Font.font("Arial", FontWeight.BOLD, 20));
 			title.setTextFill(Color.CADETBLUE);
@@ -31,7 +38,6 @@ public class Main extends Application {
 			FlowPane root = new FlowPane();
 			root.setAlignment(Pos.CENTER); //set table to be in center of the window
 			
-			Scene scene = new Scene(root,400,400);
 			
 			//Create an "observable" list of Contacts
 			ObservableList<Contact> contactList = FXCollections.observableArrayList(
@@ -49,14 +55,67 @@ public class Main extends Application {
 			
 			
 			//create a tableView for contacts:
-			TableView<Contact> myTableView;
+			TableView<Contact> tvContacts;
 			
 			//add contacts to TableView:
-			myTableView = new TableView<Contact>(contactList);
+			tvContacts = new TableView<Contact>(contactList);
 			
-			//TableView
+			//====================================IMPORTANT!!
+			//Associate the columns with a column heading:
 			
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			//The 1st column is for the first name:
+			TableColumn<Contact, String>fName = new TableColumn<>("First Name");
+			
+			
+			//use the CELL FACTORY to populate the column cells with the data:
+			fName.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+			tvContacts.getColumns().add(fName);
+			
+			//--------------
+			
+			//The 2nd column is for the last name:
+			TableColumn<Contact, String>lName = new TableColumn<>("Last Name");
+			
+			//use the CELL FACTORY to populate the column cells with the data:
+			lName.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+			tvContacts.getColumns().add(lName);
+			
+			//--------------
+			
+			//The 3rd column is for the phone number:
+			TableColumn<Contact, String>cell = new TableColumn<>("Phone Number");
+			
+			//use the CELL FACTORY to populate the column cells with the data:
+			cell.setCellValueFactory(new PropertyValueFactory<>("cellPhone"));
+			tvContacts.getColumns().add(cell);
+			
+			//====================================
+			
+			//set preferred width & height:
+			tvContacts.setPrefWidth(300);
+			tvContacts.setPrefHeight(300);
+			
+			//TableView SelectionModel (adding an event listener with associated behavior to table cells) +++++++++++++
+			//create the SelectionModel:
+			TableView.TableViewSelectionModel<Contact> tvSelContact = tvContacts.getSelectionModel();
+			
+			//add the listener to the selectionModel:
+			tvSelContact.selectedIndexProperty().addListener(new ChangeListener<Number>() {
+				
+				//override the ChangeListener's changed method:
+				@Override
+				public void changed(ObservableValue<? extends Number> changed, Number oldVal, Number newVal) {
+					
+					int index = (int)newVal;
+					response.setText ("The cell number for the contact selected is: " + contactList.get(index).getCellPhone());
+				}
+				
+			});
+			
+			//add labels and TABLEVIEW to root:
+			root.getChildren().addAll(title, tvContacts, response);
+			
+			Scene scene = new Scene(root,400,400);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
